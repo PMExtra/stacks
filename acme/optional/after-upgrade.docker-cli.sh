e6bfe7799fd37699f6715bd3166485af03f8b326
PMExtra <pm@jubeat.net>
2025-09-21 17:00:46 +0800

    acme: improve docker-cli script

--- acme/optional/after-upgrade.docker-cli.sh
+++ acme/optional/after-upgrade.docker-cli.sh
@@ -6,11 +6,8 @@ fi
 
 # shellcheck disable=SC2016
 _docker_compose exec -T acme.sh sh -c '
-set -e
-cp -n /etc/apk/repositories /etc/apk/repositories.bak || true
-if [ -n "${ALPINE_MIRROR}" ]; then
-  sed -i -E "s|https?://[^/]*/|${ALPINE_MIRROR}|g" /etc/apk/repositories
+if [ -n "${ALPINE_MIRROR:-}" ]; then
+  sed -i.bak -E "s|https?://[^/]*/|${ALPINE_MIRROR}|g" /etc/apk/repositories
 fi
-apk update
-apk add docker-cli
+apk update && apk add docker-cli
 '
