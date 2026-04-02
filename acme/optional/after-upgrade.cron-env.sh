1ed9d3fb1f4d8c1d478fc1f058a68d082286673f
PMExtra <pm@jubeat.net>
2026-04-03 02:33:13 +0800

    acme: fix cron env

--- acme/optional/after-upgrade.cron-env.sh
+++ acme/optional/after-upgrade.cron-env.sh
@@ -7,5 +7,10 @@ fi
 # shellcheck disable=SC2016
 _docker_compose cp -L .env acme.sh:/.dockerenv
 _docker_compose exec -T acme.sh sh -c '
-sed -i -E "s|(([0-9*]+ +){5}).*(\"/root/.acme.sh\"/acme.sh)|\1set -o allexport; source /.dockerenv; set +o allexport; \3|" /etc/crontabs/root
+grep -qF "set -o allexport" /.dockerenv || \
+  sed -i "1i set -o allexport" /.dockerenv; \
+grep -qF "set +o allexport" /.dockerenv || \
+  sed -i "\$a set +o allexport" /.dockerenv; \
+grep -qF "source /.dockerenv;" /etc/crontabs/root || \
+  sed -i -E "s|(([0-9*]+ +){5}) *(.*/acme.sh)|\1source /.dockerenv; \3|" /etc/crontabs/root
 '
